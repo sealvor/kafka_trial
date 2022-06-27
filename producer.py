9ef0ece7ac80878263a3e5bc0b73f585081ffd6c
sealvor <sealvor@gmail.com>
2022-06-27 23:42:30 +0200

    Implement consumer

--- producer.py
+++ producer.py
@@ -1,4 +1,25 @@
 from kafka import KafkaProducer
+import json
+import time
+from data import get_registered_user
 
 
-producer = KafkaProducer(bootstrap_server=["localhost:9092"])
+def json_serializer(data):
+    return json.dumps(data).encode("utf-8")
+
+
+# def get_partition(key, all, available):
+#     return 0
+
+ 
+producer = KafkaProducer(bootstrap_servers=["localhost:9092"],
+                         value_serializer=json_serializer)
+                        #  partitioner=get_partition)
+
+
+if __name__ == "__main__":
+    while 1==1:
+        registered_user = get_registered_user()
+        print(registered_user)
+        producer.send("registered_user", registered_user)
+        time.sleep(4)
